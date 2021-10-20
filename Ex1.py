7533f3a342ac120ab72ebdc8b844abbc83edf279
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2021-10-20 16:22:28 -0300

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -2,22 +2,17 @@ texto_salto = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto']
 
 saltos_do_atleta = [0.0, 0.0, 0.0, 0.0, 0.0]
 
-
 atleta = input("Atleta: ")
 
 if atleta != '':
-    for c in range(0, 5):
+           for c in range(0, 5):
         saltos_do_atleta[c] = float(input(f"{texto_salto[c]} salto: "))
 
     # Ordenando a lista de saltos
     saltos_do_atleta.sort()
-
-
     media_saltos = (saltos_do_atleta[1] + saltos_do_atleta[2] + saltos_do_atleta[3])/3
-
     print("="*30)
-
-    print(f"Media dos demais saltos.: {media_saltos:.2f}\n")
+    print(f"Media dos outros saltos.: {media_saltos:.2f}\n")
     print("Resultado final: ")
     print(f"{atleta}: {media_saltos:.2f}")
 else:
