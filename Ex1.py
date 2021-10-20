0391c49f58f09fb904454f6a740aa9aa635d2f2e
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2021-10-20 17:21:10 -0300

    AjeitandoCod

--- Ex1.py
+++ Ex1.py
@@ -1,19 +1,16 @@
-texto_salto = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto']
+saltos_do_atleta = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto']
 
-saltos_do_atleta = [0.0, 0.0, 0.0, 0.0, 0.0]
-
+distância_saltos = [0.0, 0.0, 0.0, 0.0, 0.0]
 atleta = input("Atleta: ")
-
 if atleta != '':
-           for c in range(0, 5):
-        saltos_do_atleta[c] = float(input(f"{texto_salto[c]} salto: "))
+    for salto in range(0, 5):
+    distância_saltos[salto] = float(input(f"{texto_salto[salto]} salto: "))
 
-    # Ordenando a lista de saltos
-    saltos_do_atleta.sort()
-    media_saltos = (saltos_do_atleta[1] + saltos_do_atleta[2] + saltos_do_atleta[3])/3
-    print("="*30)
-    print(f"Media dos outros saltos.: {media_saltos:.2f}\n")
+    distância_saltos.sort()
+    media_saltos = (distância_saltos[1] + distância_saltos[2] + distância_saltos[3])/3
+
     print("Resultado final: ")
-    print(f"{atleta}: {media_saltos:.2f}")
+    print(f"Atleta: {atleta}")
+    print(f"Media de saltos.: {media_saltos:.2f}\n")
 else:
     print('Informe o nome do atleta')
